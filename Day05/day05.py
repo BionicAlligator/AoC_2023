c5f11b0d32451a13d30fdf16b232de71f0cccff3
Andrew Gillard <andrew.gillard@gmail.com>
2023-12-05 09:01:20 +0000

    Day 5 Part 1 complete

--- Day05/day05.py
+++ Day05/day05.py
@@ -1,6 +1,23 @@
-TESTING = True
+import re
+
+TESTING = False
 PART = 1
 OUTPUT_TO_CONSOLE = True
+
+
+class Mapping:
+    def __init__(self, map_from, map_to, source_start, dest_start, mapping_length):
+        self.map_from = map_from
+        self.map_to = map_to
+        self.source_start = source_start
+        self.dest_start = dest_start
+        self.mapping_length = mapping_length
+
+    def covers(self, value):
+        return value in range(self.source_start, self.source_start + self.mapping_length)
+
+    def map(self, value):
+        return value + self.dest_start - self.source_start
 
 
 def log(message, end="\n"):
@@ -20,7 +37,7 @@ def read_tests(test_filename):
             expected = line.split("=")[1].lstrip()
         elif line.rstrip().endswith("="):
             expected = line.split("=")[0].rstrip()
-        elif not line.strip():  # Blank line means end of test specification
+        elif line.strip() == "END":  # "END" means end of test specification
             tests.append((expected, inputs.copy()))
             inputs = []
         else:
@@ -35,9 +52,73 @@ def read_input(filename):
     return lines
 
 
-def part1(inputs):
+def parse_input(inputs):
+    seeds = []
+    mappings = []
 
-    return
+    map_from = map_to = ""
+
+    for line in inputs:
+        if seed_info := re.search('seeds: (.*)', line):
+            seeds = [int(seed_string) for seed_string in seed_info.group(1).split()]
+            log(f"{seeds=}")
+        elif mapping_init := re.search('(.*?)-to-(.*?) map', line):
+            map_from = mapping_init.group(1)
+            map_to = mapping_init.group(2)
+            log(f"Mapping from {map_from} to {map_to}")
+        elif mapping_spec := re.search('(\d+) (\d+) (\d+)', line):
+            dest_start = (int)(mapping_spec.group(1))
+            source_start = (int)(mapping_spec.group(2))
+            mapping_length = (int)(mapping_spec.group(3))
+            mappings.append(Mapping(map_from, map_to, source_start, dest_start, mapping_length))
+
+    return seeds, mappings
+
+
+def determine_seed_locations(seeds, mappings):
+    seed_locations = []
+
+
+    for seed in seeds:
+        value = seed
+
+        map_from = "seed"
+
+        while not map_from == "location":
+            new_map_from = ""
+            new_value = value
+
+            for mapping in mappings:
+                if mapping.map_from == map_from:
+                    new_map_from = mapping.map_to
+
+                    if mapping.covers(value):
+                        new_value = mapping.map(value)
+
+            map_from = new_map_from
+            value = new_value
+
+        location = value
+
+        seed_locations.append((location, seed))
+
+    return seed_locations
+
+
+def seed_at_lowest_location(seed_locations):
+    nearest_seed = (float('inf'), -1)
+
+    for location, seed in seed_locations:
+        if location < nearest_seed[0]:
+            nearest_seed = (location, seed)
+
+    return nearest_seed
+
+
+def part1(inputs):
+    seeds, mappings = parse_input(inputs)
+    seed_locations = determine_seed_locations(seeds, mappings)
+    return seed_at_lowest_location(seed_locations)[0]
 
 
 def part2(inputs):
