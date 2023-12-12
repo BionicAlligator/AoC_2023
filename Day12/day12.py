c7547cb187c6e66169fba1eaa51b35e837f173bd
Andrew Gillard <andrew.gillard@gmail.com>
2023-12-12 06:45:42 +0000

    Day 12 Part 1 complete

--- Day12/day12.py
+++ Day12/day12.py
@@ -1,4 +1,6 @@
-TESTING = True
+import re
+
+TESTING = False
 PART = 1
 OUTPUT_TO_CONSOLE = True
 
@@ -35,8 +37,77 @@ def read_input(filename):
     return lines
 
 
+def parse_input(inputs):
+    springs = []
+    patterns = []
+
+    for row in inputs:
+        spring_string, broken_spring_string = row.split(" ")
+
+        springs.append(spring_string.replace('.', 'g').replace('?', 'u').replace('#', 'b'))
+
+        broken_springs = broken_spring_string.split(',')
+
+        pattern = "g*?"
+
+        for index, spring in enumerate(broken_springs):
+            if index == len(broken_springs) - 1:
+                pattern += "b{" + spring + "}g*?$"
+            else:
+                pattern += "b{" + spring + "}g+?"
+
+        patterns.append(pattern)
+
+    return springs, patterns
+
+
+def generate_spring_arrangements(spring_row):
+    spring_arrangements = []
+
+    if 'u' not in spring_row:
+        return [spring_row]
+
+    for spring_arrangement in generate_spring_arrangements(spring_row[1:]):
+        if spring_row[0] == 'u':
+            spring_arrangements.append("g" + spring_arrangement)
+            spring_arrangements.append("b" + spring_arrangement)
+        else:
+            spring_arrangements.append(spring_row[0] + spring_arrangement)
+
+    return spring_arrangements
+
+
+def determine_possible_arrangements(springs, patterns):
+    possible_arrangements = []
+
+    for row_num in range(len(springs)):
+        possible_arrangements.append([])
+
+        spring_row = springs[row_num]
+        pattern = patterns[row_num]
+
+        all_spring_arrangements = generate_spring_arrangements(spring_row)
+
+        for spring_arrangement in all_spring_arrangements:
+            if re.match(pattern, spring_arrangement):
+                possible_arrangements[row_num].append(spring_arrangement)
+
+    return possible_arrangements
+
+
+def count_possible_arrangements(possible_arrangements):
+    total = 0
+
+    for spring_row_arrangements in possible_arrangements:
+        total += len(spring_row_arrangements)
+
+    return total
+
+
 def part1(inputs):
-    return
+    springs, patterns = parse_input(inputs)
+    possible_arrangements = determine_possible_arrangements(springs, patterns)
+    return count_possible_arrangements(possible_arrangements)
 
 
 def part2(inputs):
