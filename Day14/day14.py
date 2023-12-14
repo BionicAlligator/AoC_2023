ad8286dfff4213a58057aee96726eec76a1adaf2
Andrew Gillard <andrew.gillard@gmail.com>
2023-12-14 05:41:20 +0000

    Day 14 Part 1 complete

--- Day14/day14.py
+++ Day14/day14.py
@@ -1,4 +1,4 @@
-TESTING = True
+TESTING = False
 PART = 1
 OUTPUT_TO_CONSOLE = True
 
@@ -35,8 +35,85 @@ def read_input(filename):
     return lines
 
 
+def parse_input(inputs):
+    platform = []
+
+    for y, row in enumerate(inputs):
+        platform.append([])
+
+        for x, pos in enumerate(row):
+            platform[y].append(pos)
+
+    return platform
+
+
+def transpose(platform):
+    return [[row[i] for row in platform] for i in range(len(platform[0]))]
+
+
+def tilt(platform, direction="north"):
+    tilted_platform = []
+
+    if direction == "north":
+        platform = transpose(platform)
+
+        for row_num, row in enumerate(platform):
+            tilted_platform.append([])
+            empty_positions = []
+
+            for column_num, pos in enumerate(row):
+                match pos:
+                    case '#':
+                        empty_positions = []
+                        tilted_platform[row_num].append('#')
+
+                    case '.':
+                        empty_positions.append(column_num)
+                        tilted_platform[row_num].append('.')
+
+                    case 'O':
+                        if len(empty_positions) > 0:
+                            new_rock_pos = empty_positions.pop(0)
+                            tilted_platform[row_num][new_rock_pos] = 'O'
+                            empty_positions.append(column_num)
+                            tilted_platform[row_num].append('.')
+                        else:
+                            tilted_platform[row_num].append('O')
+
+        tilted_platform = transpose(tilted_platform)
+
+    log(f"{tilted_platform}")
+
+    return tilted_platform
+
+
+def extract_rock_positions(platform, rock_type='O'):
+    rock_positions = []
+
+    for row_num, row in enumerate(reversed(platform)):
+        for column_num, pos in enumerate(row):
+            if pos == rock_type:
+                rock_positions.append((row_num + 1, column_num))
+
+    log(f"{rock_positions =}")
+
+    return rock_positions
+
+
+def calc_load(rock_positions):
+    total = 0
+
+    for y, x in rock_positions:
+        total += y
+
+    return total
+
+
 def part1(inputs):
-    return
+    platform = parse_input(inputs)
+    platform_tilted_north = tilt(platform)
+    round_rock_positions = extract_rock_positions(platform_tilted_north)
+    return calc_load(round_rock_positions)
 
 
 def part2(inputs):
