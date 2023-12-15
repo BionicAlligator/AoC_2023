87afdc53b094cf7d77988793904f0b66ada66550
Andrew Gillard <andrew.gillard@gmail.com>
2023-12-15 06:27:54 +0000

    Day 16 Prep

---
+++ Day16/day16.py
@@ -0,0 +1,78 @@
+TESTING = True
+PART = 1
+OUTPUT_TO_CONSOLE = True
+
+
+def log(message, end="\n"):
+    if OUTPUT_TO_CONSOLE:
+        print(message, end=end)
+
+
+def read_tests(test_filename):
+    tests = []
+    inputs = []
+    expected = ""
+
+    file = open(test_filename, "r")
+
+    for line in file:
+        if line.lstrip().startswith("="):  # Line with equals sign at beginning or end means it is the expected output
+            expected = line.split("=")[1].lstrip()
+        elif line.rstrip().endswith("="):
+            expected = line.split("=")[0].rstrip()
+        elif not line.strip():  # Blank line means end of test specification
+            tests.append((expected, inputs.copy()))
+            inputs = []
+        else:
+            inputs.append(line.rstrip())
+
+    return tests
+
+
+def read_input(filename):
+    file = open(filename, "r")
+    lines = [line.rstrip() for line in file]
+    return lines
+
+
+def part1(inputs):
+    return
+
+
+def part2(inputs):
+    return
+
+
+def run_tests():
+    tests = read_tests("sample_input_part_" + str(PART) + ".txt")
+
+    print(f"Test Results for Part {PART}")
+
+    passed = failed = 0
+
+    for expected, inputs in tests:
+        actual = part1(inputs) if PART == 1 else part2(inputs)
+
+        if expected == str(actual):
+            passed += 1
+            print(f"Passed: {inputs} -> {actual}\n")
+        else:
+            failed += 1
+            print(f"Failed: {inputs} -> {actual}, expected {expected}\n")
+
+    print(f"Passed: {passed}, Failed: {failed}")
+
+
+def run_for_real():
+    inputs = read_input("input.txt")
+
+    print("Part 1: ", part1(inputs))
+
+    if PART == 2:
+        print("Part 2: ", part2(inputs))
+
+
+if TESTING:
+    run_tests()
+else:
+    run_for_real()
