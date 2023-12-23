82546e8020714272c08ab1ea36dedf7844c95c44
Andrew Gillard <BionicAlligator@users.noreply.github.com>
2023-12-23 10:56:54 +0100

    Day 23 Part 1 and Part 2 complete (but slow)

--- Day23/day23.py
+++ Day23/day23.py
@@ -1,6 +1,53 @@
-TESTING = True
-PART = 1
+from collections import deque
+
+TESTING = False
+PART = 2
 OUTPUT_TO_CONSOLE = True
+
+OFFSETS = {'>': (0, 1), '<': (0, -1), 'v': (1, 0), '^': (-1, 0)}
+
+
+class Node:
+    def __init__(self, pos, desc, neighbour_coords):
+        y, x = pos
+        self.y = y
+        self.x = x
+        self.desc = desc
+        self.neighbour_coords = neighbour_coords
+        self.neighbours = {}
+
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
+    def link(self, nodes_by_coord):
+        # if self.desc in OFFSETS:
+        #     offset_y, offset_x = OFFSETS[self.desc]
+        #     neighbour_coord = (self.y + offset_y, self.x + offset_x)
+        #     self.neighbours = [nodes_by_coord[neighbour_coord]]
+        # else:
+        for neighbour_coord in self.neighbour_coords:
+            self.neighbours.update({nodes_by_coord[neighbour_coord]: 1})
+
+    def add_shortcut(self, neighbour):
+        dest = neighbour
+        prev = self
+
+        nodes_on_route = []
+
+        while len(dest.neighbours) == 2:
+            nodes_on_route.append(dest)
+
+            path_length_to_dest = self.neighbours.pop(dest)
+
+            next_neighbour = [{node: path_length + path_length_to_dest}
+                               for node, path_length in dest.neighbours.items()
+                               if node != prev][0]
+
+            self.neighbours.update(next_neighbour)
+            prev = dest
+            dest, path_increment = next_neighbour.popitem()
+
+        return dest, path_increment, nodes_on_route
 
 
 def log(message, end="\n"):
@@ -35,12 +82,97 @@ def read_input(filename):
     return lines
 
 
+def parse_input(grid):
+    nodes_by_coord = {}
+
+    for y, row in enumerate(grid):
+        for x, desc in enumerate(row):
+            if grid[y][x] == '#':
+                continue
+
+            neighbour_coords = []
+
+            for offset_y, offset_x in OFFSETS.values():
+                neighbour_y = y + offset_y
+                neighbour_x = x + offset_x
+
+                if 0 <= neighbour_y < len(grid) and 0 <= neighbour_x < len(row) and grid[neighbour_y][neighbour_x] != '#':
+                    neighbour_coords.append((neighbour_y, neighbour_x))
+
+            nodes_by_coord.update({(y, x): Node((y, x), desc, neighbour_coords)})
+
+    for node in nodes_by_coord.values():
+        node.link(nodes_by_coord)
+
+    start_node = nodes_by_coord[(0, 1)]
+    end_node = nodes_by_coord[(len(grid) - 1, len(grid[0]) - 2)]
+
+    return nodes_by_coord.values(), start_node, end_node
+
+
+def find_longest_path(current, end):
+    longest_path_length = 0
+    longest_path = []
+
+    to_check = deque([(0, current, [current])])
+
+    while to_check:
+        steps, current, path = to_check.pop()
+
+        if current == end:
+            if steps > longest_path_length:
+                longest_path_length = steps
+                longest_path = path
+                log(f"Path found of length {longest_path_length}, still to check: {len(to_check)}")
+                continue
+
+        neighbour_info = current.neighbours.copy()
+
+        for neighbour, path_increment in neighbour_info.items():
+            if neighbour in path:
+                continue
+
+            new_path = path.copy()
+
+            if len(neighbour.neighbours) == 2:
+                neighbour, path_increment, nodes_on_route = current.add_shortcut(neighbour)
+                new_path.extend(nodes_on_route)
+
+            new_path.append(neighbour)
+
+            to_check.append((steps + path_increment, neighbour, new_path))
+
+    return longest_path_length, longest_path
+
+
+def display_path(grid, path):
+    node_coords = []
+
+    for node in path:
+        node_coords.append((node.y, node.x))
+
+    for y, row in enumerate(grid):
+        for x, desc in enumerate(row):
+            if (y, x) in node_coords:
+                log(".", end="")
+            elif desc in ".<>v^":
+                log(" ", end="")
+            else:
+                log(desc, end="")
+
+        log("")
+
+
 def part1(inputs):
-    return
+    nodes, start_node, end_node = parse_input(inputs)
+    return find_longest_path(start_node, end_node)
 
 
 def part2(inputs):
-    return
+    nodes, start_node, end_node = parse_input(inputs)
+    longest_path_length, longest_path = find_longest_path(start_node, end_node)
+    display_path(inputs, longest_path)
+    return longest_path_length
 
 
 def run_tests():
