ba366025dbcbbebaaad3c82581805ca0c01eb402
Andrew Gillard <BionicAlligator@users.noreply.github.com>
2023-12-23 17:07:00 +0100

    Day 20 Part 1 Complete

--- Day20/day20.py
+++ Day20/day20.py
@@ -1,29 +1,128 @@
-TESTING = True
+from collections import deque
+
+TESTING = False
 PART = 1
 OUTPUT_TO_CONSOLE = True
 
+LOW = 0
+HIGH = 1
 
-# PulseTracker
-#
-# Module
-#   - FlipFlopModule (%)
+MODULE_TYPES = {'%': "flip-flop", "&": "conjunction"}
+
+
+class PulseTracker:
+    def __init__(self):
+        self.pulses = [0, 0]
+        self.pulse_queue = deque()
+
+    def transmit_pulse(self, source, destination, pulse):
+        self.pulse_queue.append((source, destination, pulse))
+
+    def process_transmissions(self):
+        while self.pulse_queue:
+            source, destination, pulse = self.pulse_queue.popleft()
+            self.pulses[pulse] += 1
+            destination.receive(source, pulse)
+
+    def pulse_product(self):
+        return self.pulses[0] * self.pulses[1]
+
+
+class Module:
+    def __init__(self, pulse_tracker, name, destination_module_names):
+        self.pulse_tracker = pulse_tracker
+        self.name = name
+        self.destination_module_names = destination_module_names
+        self.destination_modules = set()
+        self.incoming = {}
+
+    def receive(self, source, pulse):
+        self.transmit(pulse)
+
+    def transmit(self, pulse):
+        for module in self.destination_modules:
+            self.pulse_tracker.transmit_pulse(self, module, pulse)
+
+        self.pulse_tracker.process_transmissions()
+
+    def link(self, modules):
+        for dest_name in self.destination_module_names:
+            if dest_name in modules:
+                module = modules[dest_name]
+            else:
+                module = Output(self.pulse_tracker, dest_name, [])
+
+            self.destination_modules.add(module)
+            module.connect(self)
+
+    def connect(self, source):
+        self.incoming.update({source: LOW})
+
+
+class Output(Module):
+    def __init__(self, pulse_tracker, name, destination_module_names):
+        super().__init__(pulse_tracker, name, destination_module_names)
+
+    def receive(self, source, pulse):
+        pass
+
+    def transmit(self, pulse):
+        pass
+
+
+class Button(Module):
+    def __init__(self, pulse_tracker, name, destination_module_names):
+        super().__init__(pulse_tracker, name, destination_module_names)
+
+    def push(self):
+        self.receive(self, LOW)
+
+
+#   - Broadcaster (broadcaster)
+#     Re-transmits received pulse to all downstream modules
+class Broadcaster(Module):
+    def __init__(self, pulse_tracker, name, destination_module_names):
+        super().__init__(pulse_tracker, name, destination_module_names)
+
+
+#   - FlipFlop (%)
 #     Initially off
 #     Toggles state when receives low pulse
-#     When toggles on, sends high pulse
-#     When toggles off, sends low pulse
-#
-#   - ConjunctionModule (&)
+#       When toggles on, sends high pulse
+#       When toggles off, sends low pulse
+class FlipFlop(Module):
+    def __init__(self, pulse_tracker, name, destination_module_names):
+        super().__init__(pulse_tracker, name, destination_module_names)
+        self.state = LOW
+
+    def receive(self, source, pulse):
+        if pulse == LOW:
+            if self.state == LOW:
+                self.state = HIGH
+                self.transmit(HIGH)
+            else:
+                self.state = LOW
+                self.transmit(LOW)
+
+
+#   - Conjunction (&)
 #     Multiple inputs, tracks last pulse from each
 #     All inputs initially low
 #     When pulse is received:
 #       If all inputs are high, sends low pulse
 #       Else sends high pulse
-#
-#   - BroadcastModule (broadcaster)
-#     Sends received pulse to all destination modules
-#
-#   - ButtonModule (button)
-#     Sends single low pulse to broadcaster
+class Conjunction(Module):
+    def __init__(self, pulse_tracker, name, destination_module_names):
+        super().__init__(pulse_tracker, name, destination_module_names)
+
+    def receive(self, source, pulse):
+        self.incoming.update({source: pulse})
+
+        if all(val == HIGH for val in self.incoming.values()):
+            self.transmit(LOW)
+        else:
+            self.transmit(HIGH)
+
 
 def log(message, end="\n"):
     if OUTPUT_TO_CONSOLE:
@@ -57,8 +156,44 @@ def read_input(filename):
     return lines
 
 
+def parse_input(inputs, pulse_tracker):
+    modules = {}
+    broadcaster = None
+
+    for line in inputs:
+        module_info, destinations = line.split(" -> ")
+
+        module_type = "broadcaster" if module_info == "broadcaster" else MODULE_TYPES[module_info[0]]
+        module_name = "broadcaster" if module_info == "broadcaster" else module_info[1:]
+        destination_module_names = destinations.split(", ")
+
+        match module_type:
+            case "broadcaster":
+                broadcaster = Broadcaster(pulse_tracker, module_name, destination_module_names)
+                modules.update({module_name: broadcaster})
+
+            case "flip-flop":
+                modules.update({module_name: FlipFlop(pulse_tracker, module_name, destination_module_names)})
+
+            case "conjunction":
+                modules.update({module_name: Conjunction(pulse_tracker, module_name, destination_module_names)})
+
+    for module in modules.values():
+        module.link(modules)
+
+    return modules, broadcaster
+
+
 def part1(inputs):
-    return
+    pulse_tracker = PulseTracker()
+    modules, broadcaster = parse_input(inputs, pulse_tracker)
+    button = Button(pulse_tracker, "button", ["broadcaster"])
+    button.link(modules)
+
+    for push in range(1000):
+        button.push()
+
+    return pulse_tracker.pulse_product()
 
 
 def part2(inputs):
